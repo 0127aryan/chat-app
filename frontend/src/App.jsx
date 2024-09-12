b52ca351e782fd3ba4bda219a4ddc75ad5f16453
Aryan Khandelwal <aryankhandelwal0127@gmail.com>
2024-09-12 22:57:45 +0530

    12345

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 
 import { Routes, Route,Navigate } from 'react-router-dom'
-import './app.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
